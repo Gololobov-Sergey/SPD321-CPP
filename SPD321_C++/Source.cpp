fb8c078f7f2e0499be5cfa9346d03143573f7b77
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-09-09 15:32:24 +0300

    09.09.2023

--- SPD321_C++/Source.cpp
+++ SPD321_C++/Source.cpp
@@ -13,235 +13,98 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-
-	// 02.09.2023 //
-
-
-	/*cout << "Hello C++" << endl;
-	cout << "Мене звати Сергій" << endl;*/
-
-	// bool        - 1b
-
-	// char        - 1b
-
-	// short       - 2b
-	// int 
-	// long        - 4b
-	// long long   - 8b
-
-	// float       - 4b
-	// double      - 8b
-	// long double - 10b
-
-	// modify type name;
-
-	/*int year;
-	year = 2023;
-	cout << year << endl;
-
-	const int a = 5;
-
-	unsigned int b = -4;
-	cout << b << endl;*/
-
-	// -, ++, --
-	// = + - * / %, += -=  *= /= %=
-
-	// !
-	// <  > <=  >=  != == &&(and), ||(or), ^(xor)
-
-	/*int a = 5;
-	int b = 4;
-	int c;*/
-
-	/*a = a + b;
-	a += b;
-
-	a = a + 1;
-	a += 1;
-	a++;
-	++a;
-	a--;*/
-
-	/*c = ++a * (a++ - ++b);
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;*/
+	///// 09.09.2023   ///////
 
 
-	//cout << a << endl;   //5
-	//cout << ++a << endl; //6
-	//cout << a++ << endl; //6
-	//cout << a << endl;   //7
+	/*while (condition)
+	{
+		oper;
+	}
 
-	/*c = !a < b;
-	cout << c << endl;*/
+	do
+	{
+		oper;
+	} while (condition);*/
+
+	/*int a = 1;
+	while (a <= 5)
+	{
+		cout << a << " ";
+		a++;
+	}
+	cout << endl;*/
 
 
-	/*int a, b, s;
-	cout << "a = ";
+	/*int b = 1;
+	do
+	{
+		cout << b << " ";
+		b++;
+	} while (b <= 5);
+	cout << endl;*/
+
+	
+	/*for (int i = 1; i <= 5; i++)
+	{
+		cout << i << " ";
+	}
+	cout << endl;*/
+	
+	
+	/*int a, b, sum = 0;
+	cout << "Enter first range number >> ";
 	cin >> a;
-	cout << "b = ";
+	cout << "Enter second range number >> ";
 	cin >> b;
-	s = a * b;
-	cout <<  "a = " << a << ", b = " << b << ", Площа: " << s << endl;*/
-
-
-	/*int b = 5;
-	float a = (float)b / 2;
-	cout << a << endl;
-
-	float c = (5 + 6) / 2.;*/
-
-	/*char t = 'a' + 3;
-	cout << t << endl;
-	cout << (int)t << endl;*/
-
-
-	/*if (condition)
+	if (a < b) 
 	{
-		oper;
-	}
-	else
-	{
-		oper;
+		for (int i = a; i <= b; i++) 
+		{
+			sum += i;
+		}
+		cout << sum << endl;
 	}*/
 
 
-	/*int a, b, c, d, count = 0;
-	cin >> a >> b >> c >> d;
-
-	if (a % 2 == 0)
+	/*int n, sum = 0;
+	do
 	{
-		count++;
-	}
-
-	if (b % 2 == 0)
-	{
-		count++;
-	}
-
-	if (c % 2 == 0)
-	{
-		count++;
-	}
-
-	if (d % 2 == 0)
-	{
-		count++;
-	}
-	cout << count << endl;*/
+		cin >> n;
+		sum += n;
+	} while (n != 0);
+	cout << sum << endl;*/
 
 
-	/*int a, b, c;
-	cin >> a >> b >> c;
-	if (a > b && a > c)
+	/*int a, b;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++)
 	{
-		cout << a << endl;
-	}
-	else
-	{
-		if (b > c)
+		for (int j = 0; j < i; j++)
 		{
-			cout << b << endl;
+			cout << i << " ";
 		}
-		else
-		{
-			cout << c << endl;
-		}
+		cout << endl;
 	}*/
 
 
-	/*int a, b, res = 0;
-	char op;
-	cin >> a >> op >> b;*/
 
-	/*if (op == '+')
-	{
-		res = a + b;
-		cout << res << endl;
-	}
-	else
-	{
-		if (op == '-')
-		{
-			res = a - b;
-			cout << res << endl;
-		}
-		else
-		{
-			if (op == '*')
-			{
-				res = a * b;
-				cout << res << endl;
-			}
-			else
-			{
-				if (op == '/')
-				{
-					res = a / b;
-					cout << res << endl;
-				}
-				else
-				{
-					cout << "Error operation" << endl;
-				}
-			}
-		}
-	}*/
-	/*switch (op)
-	{
-	case '+': res = a + b; break;
-	case '-': res = a - b; break;
-	case '*':
-		res = a * b;
-		break;
-	case '/':
-		res = a / b;
-		break;
-	case '%' :
-		res = a % b;
-		break;
-	default:
-		cout << "Error operation" << endl;
-		break;
-	}
-	cout << res << endl;*/
+	/*int n;
+	cin >> n;
 
-
-	/*int choice;
-	cin >> choice;
-
-	switch (choice)
+	int m = n, k = 0;
+	while (m > 0) 
 	{
-	case LOAD:
-		break;
-	case SAVE:
-		break;
-	case EXIT:
-		break;
-	default:
-		break;
-	}*/
-
-	int a, b,c, max;
-	cin >> a >> b >> c;
-	if (a > b)
-	{
-		max = a;
-	}
-	else
-	{
-		max = b;
+		k++;
+		m /= 10;
 	}
 
-	// (condition)? oper1 : oper2;
-	//(a > b) ? max = a : max = b;
-	//max = (a > b) ? a : b;
+	for (int i = 0; i < 5 - k; i++)
+	{
+		cout << 0;
+	}
+	cout << n << endl;*/
 
-	max = (a > b && a > c) ? a : (b > c) ? b : c;
 
-	cout << max << endl;
 
 
 	system("pause");
